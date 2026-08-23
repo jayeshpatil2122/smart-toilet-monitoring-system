76ce031701f81d03913e09b0214fe925de359b41
jayeshpatil2122 <jayeshpatil212206@gmail.com>
2026-08-24 01:08:20 +0530

    Fix Railway static files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -90,10 +90,9 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
-
 
 # ==============================
 # URL / WSGI
